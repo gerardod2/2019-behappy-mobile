fee26826a7ade02a69efeeeb3a06812e94c39f4e
Gerardo Muniz <gerardofigueredomuniz@gmail.com>
2020-01-14 16:54:41 -0300

    Correção

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,10 @@
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import {
-    HomeScreen,
-    ProfileScreen,
-    SignInScreen,
-    SplashScreen
+  HomeScreen,
+  ProfileScreen,
+  SignInScreen,
+  SplashScreen
 } from "../screens";
 
 const APP_STACK = "App";
@@ -16,22 +16,22 @@ const PROFILE_SCREEN = "Profile";
 const SIGNIN_SCREEN = "Auth";
 
 const AppStack = createStackNavigator({
-    Home: HomeScreen,
-    Profile: ProfileScreen
+  Home: HomeScreen,
+  Profile: ProfileScreen
 });
 const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
 export { APP_STACK, AUTH_STACK, HOME_SCREEN, PROFILE_SCREEN, SIGNIN_SCREEN };
 
 export default createAppContainer(
-    createSwitchNavigator(
-        {
-            Splash: SplashScreen,
-            App: AppStack,
-            Auth: AuthStack
-        },
-        {
-            initialRouteName: SPLASH_SCREEN
-        }
-    )
+  createSwitchNavigator(
+    {
+      Splash: SplashScreen,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: SPLASH_SCREEN
+    }
+  )
 );
